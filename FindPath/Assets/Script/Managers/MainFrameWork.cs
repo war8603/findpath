bd896572959711cb5d06c4d55c26e86789cc9281
war8603 <war8603@nate.com>
2025-08-25 16:13:47 +0900

    set frame rate, screen never sleep

--- FindPath/Assets/Script/Managers/MainFrameWork.cs
+++ FindPath/Assets/Script/Managers/MainFrameWork.cs
@@ -19,6 +19,8 @@ namespace Managers
         private void Start()
         {
             Init();
+            Application.targetFrameRate = 60;
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
         }
 
         private void Init()
